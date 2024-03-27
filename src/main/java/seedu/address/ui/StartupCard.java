0fd6a16d08afa27c592e47f74f5c3aefed7e62d2
yeoshuheng <yeoshuheng@gmail.com>
2024-03-27 20:20:14 +0800

    feat: Integrate valuation to UI

--- src/main/java/seedu/address/ui/StartupCard.java
+++ src/main/java/seedu/address/ui/StartupCard.java
@@ -12,7 +12,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import seedu.address.model.startup.Startup;
-import seedu.address.model.startup.Note;
+import seedu.address.model.startup.Valuation;
 
 /**
  * An UI component that displays information of a {@code Startup}.
@@ -47,6 +47,12 @@ public class StartupCard extends UiPart<Region> {
     private FlowPane industryAndFundingStage;
 
     @FXML
+    private FlowPane valuationDisplay;
+
+    @FXML
+    private Label valuation;
+
+    @FXML
     private Label note; // Add this field for note
     @FXML
     private FlowPane tags;
@@ -62,6 +68,7 @@ public class StartupCard extends UiPart<Region> {
         phone.setText(startup.getPhone().value);
         address.setText(startup.getAddress().value);
         email.setText(startup.getEmail().value);
+        valuation.setText(startup.getValuation().value);
 
         String fundingLevel = startup.getFundingStage().value;
         if (fundingLevel.equals("PS")) {
@@ -74,6 +81,9 @@ public class StartupCard extends UiPart<Region> {
         industryAndFundingStage.getChildren().addAll(
                 new Label(startup.getIndustry().value),
                 new Label(fundingLevel));
+        valuationDisplay.getChildren().addAll(
+                new Label(startup.getValuation().value)
+        );
         startup.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
